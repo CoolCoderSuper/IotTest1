3deb3890d7fa6909b5bac47b3e12a32b7b05c613
Joseph Guenther <joseph@codingcool.ca>
2024-10-19 17:16:04 -0400

    Add project files.

---
+++ IotTest1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Device.Gpio;
+
+Console.WriteLine("Start");
+var controller = new GpioController(PinNumberingScheme.Board);
+var pin = 10;
+var lightTime = 300;
+controller.OpenPin(pin, PinMode.Output);
+try
+{
+    while (true)
+    {
+        controller.Write(pin, PinValue.High);
+        Thread.Sleep(lightTime);
+        controller.Write(pin, PinValue.Low);
+        Thread.Sleep(lightTime);
+    }
+}
+finally
+{
+    controller.ClosePin(pin);
+}
